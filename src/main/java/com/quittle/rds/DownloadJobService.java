3304e53871aa4357c141a89fc2e8ebfa28a8b808
Dustin Toff <quittleland@gmail.com>
2018-06-13 04:22:40 +0000

    Added support for stopping the download service
    * Does not always work if a pending job was in progress already
    * Major refactor to remove downloadBroadcastReceiver

--- src/main/java/com/quittle/rds/DownloadJobService.java
+++ src/main/java/com/quittle/rds/DownloadJobService.java
@@ -5,11 +5,14 @@ import android.app.job.JobInfo;
 import android.app.job.JobParameters;
 import android.app.job.JobScheduler;
 import android.app.job.JobService;
+import android.content.BroadcastReceiver;
 import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.database.Cursor;
 import android.net.Uri;
+import android.os.PersistableBundle;
 import android.util.Log;
 import com.google.common.hash.HashFunction;
 import com.google.common.hash.Hashing;
@@ -17,104 +20,86 @@ import com.google.common.io.Files;
 import java.io.File;
 import java.io.IOException;
 import java.util.Objects;
-import lombok.Data;
 
 @SuppressWarnings("PMD.AccessorMethodGeneration")
 public class DownloadJobService extends JobService {
     private static final String TAG = DownloadJobService.class.getSimpleName();
     private static final HashFunction SHA_256_HASH_FUNCTION = Hashing.sha256();
-    private static final int JOB_ID_DOWNLOAD_CHECK = 1;
+    private static final String BUNDLE_KEY_URL = "url";
 
-    private DownloadMetadata downloadMetadata;
-    private OngoingDownload ongoingDownload;
+    private static final IntentFilter DOWNLOAD_COMPLETE_INTENT_FILTER = new IntentFilter();
+    static {
+        DOWNLOAD_COMPLETE_INTENT_FILTER.addAction(DownloadManager.ACTION_DOWNLOAD_COMPLETE);
+    }
+
     private DownloadManager downloadManager;
     private PackageManagementUtils packageManagementUtils;
 
     private static String previousHash;
 
-    @Data
-    private static class OngoingDownload {
-        private long id;
-        private JobParameters params;
-        private String url;
+    /**
+     * @param context Android context used to schedule the download
+     * @param url The url to download
+     */
+    public static void startRunning(final Context context, final String url) {
+        startRunning(context, url, false);
     }
 
-    public static void startRunning(final Context context) {
+    private static void startRunning(final Context context, final String url, final boolean force) {
         final JobScheduler jobScheduler = (JobScheduler) context.getSystemService(Context.JOB_SCHEDULER_SERVICE);
         if (jobScheduler == null) {
             Log.e(TAG, "Unable to get job scheduler.");
             return;
         }
 
-        jobScheduler.schedule(new JobInfo.Builder(JOB_ID_DOWNLOAD_CHECK, new ComponentName(context, DownloadJobService.class))
+        final int jobId = urlToJobId(url);
+
+        if (!force && isJobPending(jobScheduler, jobId)) {
+            Log.d(TAG, "Job already scheduled: " + url);
+            return;
+        }
+
+        final PersistableBundle extras = new PersistableBundle();
+        extras.putString(BUNDLE_KEY_URL, url);
+
+        jobScheduler.schedule(new JobInfo.Builder(jobId, new ComponentName(context, DownloadJobService.class))
                 .setMinimumLatency(1000)
                 .setRequiredNetworkType(JobInfo.NETWORK_TYPE_ANY)
+                .setExtras(extras)
                 .build());
+    }
+
+    private static boolean isJobPending(final JobScheduler jobScheduler, final int jobId) {
+        for (final JobInfo jobInfo : jobScheduler.getAllPendingJobs()) {
+            if (jobInfo.getId() == jobId) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private static int urlToJobId(final String url) {
+        return url.hashCode();
+    }
+
+    public static void stopRunning(final Context context, final String url) {
+        stopRunning(context, urlToJobId(url));
+    }
+
+    private static void stopRunning(final Context context, final int jobId) {
+        final JobScheduler jobScheduler = (JobScheduler) context.getSystemService(Context.JOB_SCHEDULER_SERVICE);
+        if (jobScheduler == null) {
+            Log.e(TAG, "Unable to get job scheduler.");
+            return;
+        }
+
+        jobScheduler.cancel(jobId);
     }
 
     @Override
     public void onCreate() {
-        downloadMetadata = new DownloadMetadata(this);
         packageManagementUtils = new PackageManagementUtils(this);
         downloadManager = (DownloadManager) getSystemService(DOWNLOAD_SERVICE);
-
-        DownloadBroadcastReceiver.setCallback(new Runnable() {
-            @Override
-            public void run() {
-                final Uri uri = getDownloadUri(ongoingDownload.getId());
-                if (uri == null) {
-                    Log.i(TAG, "Unable to download file");
-                    reschedule();
-                    return;
-                }
-
-                final String packageName = packageManagementUtils.getApkPackageName(uri);
-                if (packageName == null) {
-                    Log.i(TAG, "Downloaded file was an invalid APK");
-                    delete(uri);
-                    reschedule();
-                    return;
-                }
-
-                downloadMetadata.setDownloadUrl(ongoingDownload.getUrl());
-
-                final String downloadedHash = hashFile(uri.getPath());
-                if (downloadedHash == null) {
-                    Log.i(TAG, "Unable to hash downloaded file. Skipping");
-                    delete(uri);
-                    reschedule();
-                    return;
-                }
-
-                Log.i(TAG, "previousHash: " + previousHash + " downloadedHash: " + downloadedHash);
-
-                if (Objects.equals(downloadedHash, previousHash)) {
-                    Log.i(TAG, "APK downloaded previously. Skipping install prompt");
-                    delete(uri);
-                    reschedule();
-                    return;
-                }
-
-                previousHash = downloadedHash;
-
-                if (Objects.equals(downloadedHash, hashFile(packageManagementUtils.getInstalledPackageApk(packageName)))) {
-                    Log.i(TAG, "APK installed with matching signature already.");
-                    delete(uri);
-                    reschedule();
-                    return;
-                }
-
-                final Uri downloadedContentUri = downloadManager.getUriForDownloadedFile(ongoingDownload.getId());
-
-                Log.i(TAG, "Installing downloaded APK");
-                Intent promptInstall = new Intent(Intent.ACTION_VIEW)
-                        .setDataAndType(downloadedContentUri, "application/vnd.android.package-archive")
-                        .setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_GRANT_READ_URI_PERMISSION);
-
-                startActivity(promptInstall);
-                reschedule();
-            }
-        });
     }
 
     private static void delete(final Uri uri) {
@@ -125,52 +110,106 @@ public class DownloadJobService extends JobService {
         }
     }
 
-    private void reschedule() {
+    private void reschedule(final JobParameters params) {
         Log.i(TAG, "Rescheduling download");
-        jobFinished(ongoingDownload.getParams(), false);
-        startRunning(this);
+        jobFinished(params, false);
+        startRunning(this, params.getExtras().getString(BUNDLE_KEY_URL), true);
     }
 
     @Override
-    public boolean onStartJob(JobParameters parameters) {
-        final String url = downloadMetadata.getDownloadUrl();
+    @SuppressWarnings("PMD.AvoidFinalLocalVariable")
+    public boolean onStartJob(final JobParameters parameters) {
+        final String url = parameters.getExtras().getString(BUNDLE_KEY_URL);
         if (url == null) {
-            reschedule();
+            reschedule(parameters);
             return false;
         }
 
-        if (ongoingDownload != null) {
-            if (url.equals(ongoingDownload.getUrl())) {
-                reschedule();
-                return false;
-            } else if (downloadManager.remove(ongoingDownload.getId()) != 1) {
-                Log.e(TAG, "Unable to cancel ongoing download.");
-            }
-        }
-
-        ongoingDownload = new OngoingDownload();
-        ongoingDownload.setUrl(url);
-        ongoingDownload.setParams(parameters);
+        final long downloadId;
         try {
-            final long downloadId = downloadManager.enqueue(
+            downloadId = downloadManager.enqueue(
                     new DownloadManager.Request(Uri.parse(url))
-                            .setDestinationInExternalFilesDir(this, null, "3-download.apk"));
-            ongoingDownload.setId(downloadId);
+                            .setDestinationInExternalFilesDir(this, null, "download.apk"));
             Log.i(TAG, "Enqued download of " +  url + " with id " + downloadId);
         } catch (IllegalArgumentException e) {
             Log.w(TAG, "Unable to enqueue download", e);
-            reschedule();
+            reschedule(parameters);
             return false;
         }
+        getApplicationContext().registerReceiver(new BroadcastReceiver() {
+                @Override
+                public void onReceive(Context context, Intent intent) {
+                    Log.d(TAG, "Download received");
+                    final Long receivedDownloadId = intent.getExtras().getLong(DownloadManager.EXTRA_DOWNLOAD_ID);
+                    if (receivedDownloadId != downloadId) {
+                        return;
+                    }
+
+                    getApplicationContext().unregisterReceiver(this);
+                    onDownloadComplete(parameters, receivedDownloadId);
+                }
+            }, DOWNLOAD_COMPLETE_INTENT_FILTER);
         return true;
     }
 
     @Override
     public boolean onStopJob(JobParameters parameters) {
-        startRunning(this);
+        Log.i(TAG, "Stopping job");
         return false;
     }
 
+    private void onDownloadComplete(final JobParameters jobParameters, final long downloadId) {
+        final Uri uri = getDownloadedFileUriByDownloadId(downloadId);
+        if (uri == null) {
+            Log.i(TAG, "Unable to download file");
+            reschedule(jobParameters);
+            return;
+        }
+
+        final String packageName = packageManagementUtils.getApkPackageName(uri);
+        if (packageName == null) {
+            Log.i(TAG, "Downloaded file was an invalid APK");
+            delete(uri);
+            reschedule(jobParameters);
+            return;
+        }
+
+        final String downloadedHash = hashFile(uri.getPath());
+        if (downloadedHash == null) {
+            Log.i(TAG, "Unable to hash downloaded file. Skipping");
+            delete(uri);
+            reschedule(jobParameters);
+            return;
+        }
+
+        Log.d(TAG, "previousHash: " + previousHash + " downloadedHash: " + downloadedHash);
+
+        if (Objects.equals(downloadedHash, previousHash)) {
+            Log.i(TAG, "APK downloaded previously. Skipping install prompt");
+            delete(uri);
+            reschedule(jobParameters);
+            return;
+        }
+
+        previousHash = downloadedHash;
+
+        if (Objects.equals(downloadedHash, hashFile(packageManagementUtils.getInstalledPackageApk(packageName)))) {
+            Log.i(TAG, "APK installed with matching signature already.");
+            delete(uri);
+            reschedule(jobParameters);
+            return;
+        }
+
+        final Uri downloadedContentUri = downloadManager.getUriForDownloadedFile(downloadId);
+
+        Log.i(TAG, "Installing downloaded APK");
+        Intent promptInstall = new Intent(Intent.ACTION_VIEW)
+                .setDataAndType(downloadedContentUri, "application/vnd.android.package-archive")
+                .setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_GRANT_READ_URI_PERMISSION);
+
+        startActivity(promptInstall);
+        reschedule(jobParameters);
+    }
 
     private String hashFile(final String path) {
         final File file = new File(path);
@@ -184,7 +223,7 @@ public class DownloadJobService extends JobService {
         }
     }
 
-    private Uri getDownloadUri(long downloadId) {
+    private Uri getDownloadedFileUriByDownloadId(long downloadId) {
         final Cursor cursor = downloadManager.query(
                 new DownloadManager.Query().setFilterById(downloadId)
                         .setFilterByStatus(DownloadManager.STATUS_SUCCESSFUL));
--- src/main/java/com/quittle/rds/MainActivity.java
+++ src/main/java/com/quittle/rds/MainActivity.java
@@ -1,12 +1,21 @@
 package com.quittle.rds;
 
 import android.app.Activity;
+import android.content.Context;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
+import android.util.Log;
 import android.widget.EditText;
+import android.widget.CompoundButton;
+import android.widget.ToggleButton;
 
+@SuppressWarnings("PMD.AccessorMethodGeneration")
 public class MainActivity extends Activity {
+    private static final String TAG = MainActivity.class.getSimpleName();
+
+    private static final boolean DOWNLOAD_ON_START_UP = true;
+
     private DownloadMetadata downloadMetadata;
 
     @Override
@@ -15,27 +24,47 @@ public class MainActivity extends Activity {
         setContentView(R.layout.main);
 
         downloadMetadata = new DownloadMetadata(this);
-        DownloadJobService.startRunning(this);
+        final String previousUrl = downloadMetadata.getDownloadUrl();
+        if (previousUrl != null && DOWNLOAD_ON_START_UP) {
+            DownloadJobService.startRunning(this, downloadMetadata.getDownloadUrl());
+        }
 
         final EditText urlEditText = findViewById(R.id.url_edit_text);
         urlEditText.addTextChangedListener(new TextWatcher() {
             @Override
-            public void beforeTextChanged(CharSequence p1, int p2, int p3, int p4) {
+            public void beforeTextChanged(CharSequence sequence, int start, int count, int after) {
+                final String text = sequence.toString().trim();
+                Log.d(TAG, "Cancelling: " + text);
+                DownloadJobService.stopRunning(MainActivity.this, text);
+            }
+
+            @Override
+            public void onTextChanged(CharSequence sequence, int start, int before, int count) {
                 // Not needed
             }
 
             @Override
-            public void onTextChanged(CharSequence p1, int p2, int p3, int p4) {
-                // Not needed
-            }
-
-            @Override
-            @SuppressWarnings("PMD.AccessorMethodGeneration")
             public void afterTextChanged(Editable editable) {
                 final String text = editable.toString().trim();
                 downloadMetadata.setDownloadUrl(text);
+                DownloadJobService.startRunning(MainActivity.this, text);
             }
         });
         urlEditText.setText(downloadMetadata.getDownloadUrl());
+
+        final ToggleButton toggleServiceButton = findViewById(R.id.toggle_service_button);
+        toggleServiceButton.setChecked(DOWNLOAD_ON_START_UP);
+        toggleServiceButton.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                final Context context = MainActivity.this;
+                final String url = downloadMetadata.getDownloadUrl();
+
+                if (isChecked) {
+                    DownloadJobService.startRunning(context, url);
+                } else {
+                    DownloadJobService.stopRunning(context, url);
+                }
+            }
+        });
     }
 }
